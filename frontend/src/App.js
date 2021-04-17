f01261910d13f72aaf22f6af5d008c589d9aae3a
skcheng <skcheng@stud.ntnu.no>
2021-04-17 14:15:47 +0200

    Started on mainapge

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,18 @@
 import React from "react";
-import './App.css';
+import "./App.css";
+import MainPage from "./pages/MainPage";
+import About from "./pages/About";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 const App = () => {
   return (
-    <div className="App">
-     
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/" exact component={MainPage} />
+        <Route path="/about" exact component={About} />
+      </Switch>
+    </Router>
   );
-}
+};
 
 export default App;
---
+++ frontend/src/pages/MainPage.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./css/mainpage.css";
+import image from "../images/training.jpg";
+
+export const MainPage = () => {
+  return (
+    <>
+    <body></body>
+
+    <div>hello</div>
+      
+    </>
+  );
+};
+
+export default MainPage;
