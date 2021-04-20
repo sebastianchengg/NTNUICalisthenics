049f2d08f3b1be996d4817a257ca7022b41d3486
skcheng <skcheng@stud.ntnu.no>
2021-04-20 23:14:56 +0200

    Added text and button on mainpage

--- frontend/src/pages/MainPage.js
+++ frontend/src/pages/MainPage.js
@@ -1,10 +1,24 @@
 import React from "react";
 import { MainPicture } from "../components/BackgroundPicture";
+import "./MainPage.css";
+import { LinkButton } from "../components/LinkButton";
 
 export const MainPage = () => {
   return (
     <>
-      <MainPicture photographer="Foto: Nils Dittrich/NTNUI" />
+      <MainPicture url="url-1" photographer="Foto: Nils Dittrich/NTNUI" />
+      <div className="main-text fade-in">
+        Master your own bodyweight
+        <br />
+        <LinkButton
+          extraCss="apply-text"
+          url="/apply"
+          buttonStyle="btn--outline"
+          buttonSize="btn--large"
+        >
+          Apply now <i className="fas fa-long-arrow-alt-right"></i>
+        </LinkButton>
+      </div>
     </>
   );
 };
