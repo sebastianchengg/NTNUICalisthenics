defeb09b45755d50dac17472c63c894dae016247
skcheng <skcheng@stud.ntnu.no>
2021-04-20 23:14:28 +0200

    Added more reuseability

--- frontend/src/components/BackgroundPicture.js
+++ frontend/src/components/BackgroundPicture.js
@@ -1,11 +1,23 @@
 import React from "react";
 import "./BackgroundPicture.css";
 
-export const MainPicture = (props) => {
+export const MainPicture = ({ url, photographer }) => {
   return (
     <>
-      <div className="main-picture-container" />
-      <div className="photographer">{props.photographer}</div>
+      <div className={`picture-container ${url}`}>
+        <div className="photographer">{photographer}</div>
+      </div>
+    </>
+  );
+};
+
+export const HalfPicture = ({ url, photographer, titleText }) => {
+  return (
+    <>
+      <div className={`picture-container half ${url}`}>
+        {titleText ? <div className="title">{titleText}</div> : null}
+        <div className="photographer">{photographer}</div>
+      </div>
     </>
   );
 };
