e7e456ebfca80687a231eaa3621de5a5631e25d1
skcheng <skcheng@stud.ntnu.no>
2021-04-22 23:15:16 +0200

    worked on animation

--- frontend/src/components/BackgroundPicture.js
+++ frontend/src/components/BackgroundPicture.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./BackgroundPicture.css";
 
+
 export const MainPicture = ({ url, photographer }) => {
   return (
     <>
@@ -12,10 +13,20 @@ export const MainPicture = ({ url, photographer }) => {
 };
 
 export const HalfPicture = ({ url, photographer, titleText }) => {
+  document.body.classList.add("js-loading");
+
+  function show() {
+    document.body.classList.add("js-loading");
+    document.body.classList.remove("js-loading");
+  }
+
+  window.addEventListener("click", show);
+  window.addEventListener("load", show);
+
   return (
     <>
       <div className={`picture-container half ${url}`}>
-        {titleText ? <div className="title title-fade-in">{titleText}</div> : null}
+        {titleText ? <div className="title fade-in">{titleText}</div> : null}
         <div className="photographer">{photographer}</div>
       </div>
     </>
--- frontend/src/pages/MainPage.js
+++ frontend/src/pages/MainPage.js
@@ -1,23 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { MainPicture } from "../components/BackgroundPicture";
 import "./MainPage.css";
 import { LinkButton } from "../components/LinkButton";
 
 export const MainPage = () => {
+  document.body.classList.add("js-loading");
+
+  function show() {
+    document.body.classList.add("js-loading");
+    document.body.classList.remove("js-loading");
+  }
+
+  window.addEventListener("click", show);
+  window.addEventListener("load", show);
+
   return (
     <>
       <MainPicture url="url-1" photographer="Foto: Nils Dittrich/NTNUI" />
       <div className="main-text fade-in">
         Master your own bodyweight
         <br />
-        <LinkButton
-          extraCss="apply-text"
-          url="/apply"
-          buttonStyle="btn--outline"
-          buttonSize="btn--large"
-        >
-          Apply now <i className="fas fa-long-arrow-alt-right"></i>
-        </LinkButton>
+          <LinkButton
+            extraCss="apply-text"
+            url="/apply"
+            buttonStyle="btn--outline"
+            buttonSize="btn--large"
+          >
+            Apply now <i className="fas fa-long-arrow-alt-right"></i>
+          </LinkButton>
       </div>
     </>
   );
