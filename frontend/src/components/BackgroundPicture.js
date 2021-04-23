03ce49ea0b40f8dd81e520ee0eb37666d4ebc39f
skcheng <skcheng@stud.ntnu.no>
2021-04-23 23:13:46 +0200

    Fixed animation

--- frontend/src/components/BackgroundPicture.js
+++ frontend/src/components/BackgroundPicture.js
@@ -1,20 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useLocation } from "react-router-dom";
 import "./BackgroundPicture.css";
 
-export const smoothAnimation = () => {
+export const SmoothAnimation = () => {
+  document.body.classList.add("js-loading");
+
   function show() {
     document.body.classList.add("js-loading");
     document.body.classList.remove("js-loading");
   }
 
-  document.body.classList.add("js-loading");
-
-  window.addEventListener("click", show);
   window.addEventListener("load", show);
+  window.addEventListener("click", show);
+  window.addEventListener("popstate", show);
 };
 
 export const MainPicture = ({ url, photographer }) => {
-  smoothAnimation();
+  SmoothAnimation();
 
   return (
     <>
@@ -26,7 +28,7 @@ export const MainPicture = ({ url, photographer }) => {
 };
 
 export const HalfPicture = ({ url, photographer, titleText }) => {
-  smoothAnimation();
+  SmoothAnimation();
 
   return (
     <>
