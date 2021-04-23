6cd5444714295dbb5498cb4d5e0f41caffd35b1e
skcheng <skcheng@stud.ntnu.no>
2021-04-23 23:13:25 +0200

    Removed unnecessary code

--- frontend/src/pages/MainPage.js
+++ frontend/src/pages/MainPage.js
@@ -1,24 +1,23 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { MainPicture } from "../components/BackgroundPicture";
 import "./MainPage.css";
 import { LinkButton } from "../components/LinkButton";
 
 export const MainPage = () => {
-
   return (
     <>
       <MainPicture url="url-1" photographer="Foto: Nils Dittrich/NTNUI" />
       <div className="main-text fade-in">
         Master your own bodyweight
         <br />
-          <LinkButton
-            extraCss="apply-text"
-            url="/apply"
-            buttonStyle="btn--outline"
-            buttonSize="btn--large"
-          >
-            Apply now
-          </LinkButton>
+        <LinkButton
+          extraCss="apply-text"
+          url="/apply"
+          buttonStyle="btn--outline"
+          buttonSize="btn--large"
+        >
+          Apply now
+        </LinkButton>
       </div>
     </>
   );
