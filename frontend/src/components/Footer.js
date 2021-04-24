d071143ff57c8085e715be8dde14122396810cdf
skcheng <skcheng@stud.ntnu.no>
2021-04-24 23:27:50 +0200

    Using reuseable social logos

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Footer.css";
+import {InstagramLogo, FacebookLogo} from "./SocialLogos";
 
 export const Footer = () => {
   return (
@@ -7,18 +8,8 @@ export const Footer = () => {
       <div className="footer-container">
         <div className="social-container">
           <div className="creator-container">2021 — Calisthenics IT-crew</div>
-          <a
-            href="https://www.instagram.com/ntnuicalisthenics/"
-            target="_blank"
-            rel="noreferrer"
-            className="fab fa-instagram spacing url"
-          ></a>
-          <a
-            href="https://www.facebook.com/ntnuicalisthenics"
-            target="_blank"
-            rel="noreferrer"
-            className="fab fa-facebook url"
-          ></a>
+          <InstagramLogo extraCss="spacing"/>
+          <FacebookLogo />
         </div>
       </div>
     </>
