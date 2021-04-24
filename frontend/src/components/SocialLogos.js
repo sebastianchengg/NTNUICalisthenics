b2cd9b6f25ea7ed6b28eed06ca104c743b946215
skcheng <skcheng@stud.ntnu.no>
2021-04-24 23:28:32 +0200

    Made reuseable social logo components

---
+++ frontend/src/components/SocialLogos.js
@@ -0,0 +1,36 @@
+import React from "react";
+import "./SocialLogos.css";
+
+export const InstagramLogo = ({ extraCss }) => {
+  return (
+    <a
+      href="https://www.instagram.com/ntnuicalisthenics/"
+      target="_blank"
+      rel="noreferrer"
+      className={`fab fa-instagram url ${extraCss}`}
+    />
+  );
+};
+
+export const FacebookLogo = ({ extraCss }) => {
+  return (
+    <a
+      href="https://www.facebook.com/ntnuicalisthenics"
+      target="_blank"
+      rel="noreferrer"
+      className={`fab fa-facebook url ${extraCss}`}
+    />
+  );
+};
+
+export const MailLogo = ({ extraCss }) => {
+  return (
+    <a
+    onClick={(e) => {
+        window.location = "mailto:calisthenics-leder@ntnui.no";
+        e.preventDefault();
+    }}
+      className={`far fa-envelope url ${extraCss}`}
+    />
+  );
+};
