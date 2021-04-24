6628c86f03d4c310b74c61b461c1b090968d6755
skcheng <skcheng@stud.ntnu.no>
2021-04-24 11:03:12 +0200

    Removed unnecessary code

--- frontend/src/components/BackgroundPicture.js
+++ frontend/src/components/BackgroundPicture.js
@@ -1,5 +1,4 @@
-import React, { useEffect } from "react";
-import { useLocation } from "react-router-dom";
+import React from "react";
 import "./BackgroundPicture.css";
 
 export const SmoothAnimation = () => {
