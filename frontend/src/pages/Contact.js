0c0e987fbd3d07eeb9b9d1f7489a51abc3071a55
skcheng <skcheng@stud.ntnu.no>
2021-04-24 11:03:41 +0200

    Made contact page

---
+++ frontend/src/pages/Contact.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { HalfPicture } from "../components/BackgroundPicture";
+
+export const Contact = () => {
+  return (
+    <>
+      <HalfPicture
+        url="url-5"
+        photographer="Foto: Nils Dittrich/NTNUI"
+        titleText="Contact"
+      />
+    </>
+  );
+};
+
+export default Contact;
