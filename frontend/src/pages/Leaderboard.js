5ad9355d938170d6ab835a09ca57301f492ff090
skcheng <skcheng@stud.ntnu.no>
2021-04-24 11:03:47 +0200

    Made leaderboard page

---
+++ frontend/src/pages/Leaderboard.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { HalfPicture } from "../components/BackgroundPicture";
+
+export const Leaderboard = () => {
+  return (
+    <>
+      <HalfPicture
+        url="url-6"
+        photographer="Foto: Roland Richter/NTNUI"
+        titleText="Leaderboard"
+      />
+    </>
+  );
+};
+
+export default Leaderboard;
