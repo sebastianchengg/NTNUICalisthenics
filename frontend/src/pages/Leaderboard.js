edbd8ab359e2cd3361923e3266a76ee534607817
skcheng <skcheng@stud.ntnu.no>
2021-04-24 23:30:56 +0200

    Started on leaderboard tables

--- frontend/src/pages/Leaderboard.js
+++ frontend/src/pages/Leaderboard.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { HalfPicture } from "../components/BackgroundPicture";
+import Grid from "@material-ui/core/Grid";
+import Table from "react-bootstrap/Table";
 
 export const Leaderboard = () => {
   return (
@@ -9,6 +11,97 @@ export const Leaderboard = () => {
         photographer="Foto: Roland Richter/NTNUI"
         titleText="Leaderboard"
       />
+      <div className="page">
+        <Grid container spacing={4}>
+          <Grid item xs={12} sm={6}>
+            <Table striped variant="dark">
+              <thead>
+                <tr>
+                  <th>Excersise</th>
+                  <th>Name</th>
+                  <th>Reps</th>
+                </tr>
+              </thead>
+
+              <tbody>
+                <tr>
+                  <td>Excersise</td>
+                  <td>Name</td>
+                  <td>Reps</td>
+                </tr>
+
+                <tr>
+                  <td>Excersise</td>
+                  <td>Name</td>
+                  <td>Reps</td>
+                </tr>
+
+                <tr>
+                  <td>Excersise</td>
+                  <td>Name</td>
+                  <td>Reps</td>
+                </tr>
+
+                <tr>
+                  <td>Excersise</td>
+                  <td>Name</td>
+                  <td>Reps</td>
+                </tr>
+
+                <tr>
+                  <td>Excersise</td>
+                  <td>Name</td>
+                  <td>Reps</td>
+                </tr>
+              </tbody>
+            </Table>
+          </Grid>
+
+          <Grid item xs={12} sm={6}>
+            <Table striped variant="dark">
+              <thead>
+                <tr>
+                  <th>Excersise</th>
+                  <th>Name</th>
+                  <th>Reps</th>
+                </tr>
+              </thead>
+
+              <tbody>
+                <tr>
+                  <td>Excersise</td>
+                  <td>Name</td>
+                  <td>Reps</td>
+                </tr>
+
+                <tr>
+                  <td>Excersise</td>
+                  <td>Name</td>
+                  <td>Reps</td>
+                </tr>
+
+                <tr>
+                  <td>Excersise</td>
+                  <td>Name</td>
+                  <td>Reps</td>
+                </tr>
+
+                <tr>
+                  <td>Excersise</td>
+                  <td>Name</td>
+                  <td>Reps</td>
+                </tr>
+
+                <tr>
+                  <td>Excersise</td>
+                  <td>Name</td>
+                  <td>Reps</td>
+                </tr>
+              </tbody>
+            </Table>
+          </Grid>
+        </Grid>
+      </div>
     </>
   );
 };
