dbff7ae7329d7a0d4ef795cae75c8b5c67fd0830
skcheng <skcheng@stud.ntnu.no>
2021-04-25 15:19:38 +0200

    Small code changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,40 +1,38 @@
 import React from "react";
-import 'bootstrap/dist/css/bootstrap.css';
+import "bootstrap/dist/css/bootstrap.css";
+import "./App.css";
+import "./pages/Apply.css";
+import "./pages/MainPage.css";
+import "./components/BackgroundPicture.css";
+import "./components/FAQComponent.css";
+import "./components/Footer.css";
+import "./components/LinkButton.css";
+import "./components/Navbar.css";
+import "./components/SocialLogos.css";
+import "./components/TextSection.css";
 import MainPage from "./pages/MainPage";
 import About from "./pages/About";
 import Apply from "./pages/Apply";
 import FAQ from "./pages/FAQ";
 import Contact from "./pages/Contact";
-import Leaderboard from "./pages/Leaderboard"
+import Leaderboard from "./pages/Leaderboard";
 import { Navbar } from "./components/Navbar";
 import { Footer } from "./components/Footer";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import "./App.css";
-import "./pages/Apply.css";
-import "./pages/MainPage.css";
-import "./components/Navbar.css";
-import "./components/LinkButton.css";
-import "./components/BackgroundPicture.css";
-import "./components/FAQComponent.css";
-import "./components/Footer.css";
-import "./components/TextSection.css";
-import "./components/SocialLogos.css";
-
-
 
 const App = () => {
   return (
     <>
       <Router>
-          <Navbar />
-          <Switch>
-            <Route path="/" exact component={MainPage} />
-            <Route path="/about" exact component={About} />
-            <Route path="/apply" exact component={Apply} />
-            <Route path="/faq" exact component={FAQ} />
-            <Route path="/contact" exact component={Contact} />
-            <Route path="/leaderboard" exact component={Leaderboard} />
-          </Switch>
+        <Navbar />
+        <Switch>
+          <Route path="/" exact component={MainPage} />
+          <Route path="/about" exact component={About} />
+          <Route path="/apply" exact component={Apply} />
+          <Route path="/faq" exact component={FAQ} />
+          <Route path="/contact" exact component={Contact} />
+          <Route path="/leaderboard" exact component={Leaderboard} />
+        </Switch>
         <Footer />
       </Router>
     </>
