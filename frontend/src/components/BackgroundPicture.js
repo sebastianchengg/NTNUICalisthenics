0be394eb439d40e96e814a71248ea9f1da736166
skcheng <skcheng@stud.ntnu.no>
2021-04-25 17:36:23 +0200

    Removed animation on normal pages

--- frontend/src/components/BackgroundPicture.js
+++ frontend/src/components/BackgroundPicture.js
@@ -5,14 +5,13 @@ export const SmoothAnimation = () => {
   document.body.classList.add("js-loading");
 
   function show() {
-    document.body.classList.add("js-loading");
     document.body.classList.remove("js-loading");
   }
 
   //All eventlisteners needed to aviod all bugs
-  window.addEventListener("load", show);
-  window.addEventListener("click", show);
-  window.addEventListener("popstate", show);
+  window.addEventListener("load", show, {once: true});
+  window.addEventListener("click", show, {once: true});
+  window.addEventListener("popstate", show, {once: true});
 };
 
 export const MainPicture = ({ url, photographer }) => {
@@ -28,12 +27,11 @@ export const MainPicture = ({ url, photographer }) => {
 };
 
 export const HalfPicture = ({ url, photographer, titleText }) => {
-  SmoothAnimation();
 
   return (
     <>
       <div className={`picture-container half ${url}`}>
-        {titleText ? <div className="title fade-in">{titleText}</div> : null}
+        {titleText ? <div className="title">{titleText}</div> : null}
         <div className="photographer">{photographer}</div>
       </div>
     </>
