9e50394729a05e451f9ad7fcf99cba66b4b6c106
skcheng <skcheng@stud.ntnu.no>
2021-05-02 15:52:00 +0200

    Added urls for tokens

--- backend/user/urls.py
+++ backend/user/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
+from rest_framework_simplejwt import views as jwt_views
 from .views import (
     SelfAPIView,
     RegisterAPIView,
 )
 
 urlpatterns = [
+    path("token/", jwt_views.TokenObtainPairView.as_view(), name="token-obtain"),
+    path("refresh/", jwt_views.TokenRefreshView.as_view(), name="token-refresh"),
     path("register/", RegisterAPIView.as_view(), name="user-register"),
     path("self/", SelfAPIView.as_view(), name="user-self"),
 ]
