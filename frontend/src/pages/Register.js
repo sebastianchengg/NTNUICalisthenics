2af8cac42f291461c2dcc401f3fac3cd298acac1
skcheng <skcheng@stud.ntnu.no>
2021-05-03 18:10:21 +0200

    Added useAuthenticationPage

--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -1,13 +1,39 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useHistory } from "react-router";
+import { useSessionContext } from "../context/session";
 import { RegisterForm } from "../components/forms/RegisterForm";
-import {TextSection} from "../components/TextSection";
+import { TextSection } from "../components/TextSection";
 import "./../App.css";
 
+/**
+ * Utility function for authentication pages. Automatically redirects
+ * a user to `/` or the set redirect path if they are already authenticated,
+ * and attempt to load this user, or the user is loaded while they are on
+ * this page.
+ */
+const useAuthenticationPage = () => {
+  const session = useSessionContext();
+  const history = useHistory();
+
+  useEffect(() => {
+    if (session.user) {
+      const redirect = session.redirectPath ?? "/";
+      history.push(redirect);
+      session.setRedirectPath("/");
+    }
+  }, [history, session]);
+};
+
 export const Register = () => {
+  useAuthenticationPage();
+
   return (
     <>
       <div className="page">
-        <TextSection title="Register" text="Register an account to be able to sign up for trainings." />
+        <TextSection
+          title="Register"
+          text="Register an account to be able to sign up for trainings."
+        />
         <RegisterForm logIn rememberLogIn />
       </div>
     </>
