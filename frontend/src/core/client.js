fe8eb8c43f3bc084d7144d036356c7855193b654
skcheng <skcheng@stud.ntnu.no>
2021-05-03 18:08:13 +0200

    Created the client the requests are sent to

---
+++ frontend/src/core/client.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import axios from "axios";
+
+export function readDjangoError(response) {
+    let errors = [];
+    for (const prop in response.data) {
+      const val = response.data[prop];
+      errors = errors.concat(val);
+    }
+  
+    if (errors.length === 0) {
+      return "En uforventet error oppstod";
+    }
+  
+    if (errors.length === 1) {
+      return errors[0];
+    }
+  
+    return (
+      <ul>
+        {errors.map((error, idx) => (
+          <li key={idx}>{error}</li>
+        ))}
+      </ul>
+    );
+  }
+
+export const API_URL = "http://localhost:8000/";
+
+/**
+ * Creates an axios instance with a set base URL
+ */
+const client = axios.create({ baseURL: API_URL });
+
+export default client;
