a73f157552bad013830da1bde07182956a4b30f3
skcheng <skcheng@stud.ntnu.no>
2021-05-05 16:44:07 +0200

    Fixed styling of Registerpage

--- frontend/src/components/forms/RegisterForm.js
+++ frontend/src/components/forms/RegisterForm.js
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
 import { useHistory } from "react-router";
 import { useSessionContext } from "../../context/session";
-import { Col, Form, InputGroup } from "react-bootstrap";
+import { Form, InputGroup } from "react-bootstrap";
 import { InternalButton } from "../LinkButton";
 import AuthenticationService from "../../core/user";
+import Grid from "@material-ui/core/Grid";
+import "./RegisterForm.css";
 
 /**
  * A form for registering a new user
@@ -61,135 +63,143 @@ export const RegisterForm = ({ logIn, rememberLogIn }) => {
   };
 
   return (
-    <Form noValidate validated={validated} onSubmit={onSubmit}>
-      <Form.Row>
-        <Col sm={4} md={3} lg={2}>
-          <Form.Label>First name</Form.Label>
-        </Col>
-        <Col sm={6} md={5} lg={4} xl={3}>
-          <Form.Control
-            autoFocus
-            type="text"
-            pattern="^[a-zA-Z\p{L}]+$"
-            minLength={2}
-            placeholder="First name"
-            onChange={(e) => setFirstName(e.target.value)}
-            required
-          />
-        </Col>
-      </Form.Row>
-      <br />
-
-      <Form.Row>
-        <Col sm={4} md={3} lg={2}>
-          <Form.Label>Last name</Form.Label>
-        </Col>
-        <Col sm={6} md={5} lg={4} xl={3}>
-          <Form.Control
-            type="text"
-            pattern="^[a-zA-Z\p{L}]+$"
-            minLength={2}
-            placeholder="Last name"
-            onChange={(e) => setLastName(e.target.value)}
-            required
-          />
-        </Col>
-      </Form.Row>
-      <br />
-
-      <Form.Row>
-        <Col sm={4} md={3} lg={2}>
-          <Form.Label>Email address</Form.Label>
-        </Col>
-        <Col sm={6} md={5} lg={4} xl={3}>
-          <Form.Control
-            type="email"
-            minLength={7}
-            placeholder="example@gmail.com"
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-        </Col>
-      </Form.Row>
-      <br />
-
-      <Form.Row>
-        <Col sm={4} md={3} lg={2}>
-          <Form.Label>Password</Form.Label>
-        </Col>
-        <Col sm={6} md={5} lg={4} xl={3}>
-          <Form.Control
-            type="password"
-            placeholder="Password"
-            minLength={8}
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            isInvalid={validated && password !== confirmPassword}
-            required
-          />
-          <Form.Text>Password has to be at least 8 characters long</Form.Text>
-        </Col>
-      </Form.Row>
-      <br />
-
-      <Form.Row>
-        <Col sm={4} md={3} lg={2}>
-          <Form.Label>Repeat password</Form.Label>
-        </Col>
-        <Col sm={6} md={5} lg={4} xl={3}>
-          <Form.Control
-            type="password"
-            placeholder="Password"
-            minLength={8}
-            value={confirmPassword}
-            onChange={(e) => setConfirmPassword(e.target.value)}
-            isInvalid={validated && password !== confirmPassword}
-            required
-          />
-        </Col>
-      </Form.Row>
-      <br />
-
-      <Form.Row>
-        <Col sm={4} md={3} lg={2}>
-          <Form.Label>Phone number</Form.Label>
-        </Col>
-        <Col xs={5} sm={3} md={2} lg={2} xl={1}>
-          <InputGroup>
-            <InputGroup.Prepend>
-              <InputGroup.Text>+</InputGroup.Text>
-            </InputGroup.Prepend>
+    <div className="register-container">
+      <Form noValidate validated={validated} onSubmit={onSubmit}>
+        <Grid container spacing={0} justify="center">
+          <Grid item xs={12} sm={4} md={5}>
+            <Form.Label>First name: </Form.Label>
+          </Grid>
+          <Grid item xs={12} sm={8} md={7}>
             <Form.Control
+              autoFocus
+              size="sm"
               type="text"
-              pattern="[0-9]*"
-              placeholder="47"
-              onChange={(e) => setCountryCode("+" + e.target.value)}
+              pattern="^[a-zA-Z\p{L}]+$"
+              minLength={2}
+              placeholder="First name"
+              onChange={(e) => setFirstName(e.target.value)}
               required
             />
-          </InputGroup>
-        </Col>
-        <Col xs={7} sm={3} md={3} lg={2} xl={2}>
-          <Form.Control
-            type="text"
-            pattern="[0-9]*"
-            minLength={8}
-            maxLength={17}
-            placeholder="99999999"
-            onChange={(e) => setPhoneNumber(e.target.value)}
-            required
-          />
-        </Col>
-      </Form.Row>
-      <br />
+          </Grid>
+          <br />
+          <br />
 
-      <InternalButton
-        buttonStyle="btn-primary"
-        buttonSize="btn-large"
-        extraCss="apply-here"
-        type="submit"
-      >
-        Registrer
-      </InternalButton>
-    </Form>
+          <Grid item xs={12} sm={4} md={5}>
+            <Form.Label>Last name: </Form.Label>
+          </Grid>
+          <Grid item xs={12} sm={8} md={7}>
+            <Form.Control
+              size="sm"
+              type="text"
+              pattern="^[a-zA-Z\p{L}]+$"
+              minLength={2}
+              placeholder="Last name"
+              onChange={(e) => setLastName(e.target.value)}
+              required
+            />
+          </Grid>
+          <br />
+          <br />
+
+          <Grid item xs={12} sm={4} md={5}>
+            <Form.Label>Email address: </Form.Label>
+          </Grid>
+          <Grid item xs={12} sm={8} md={7}>
+            <Form.Control
+              size="sm"
+              type="email"
+              minLength={7}
+              placeholder="example@gmail.com"
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </Grid>
+          <br />
+          <br />
+
+          <Grid item xs={12} sm={4} md={5}>
+            <Form.Label>Password: </Form.Label>
+          </Grid>
+          <Grid item xs={12} sm={8} md={7}>
+            <Form.Control
+              size="sm"
+              type="password"
+              placeholder="Password"
+              minLength={8}
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              isInvalid={validated && password !== confirmPassword}
+              required
+            />
+            <Form.Text>Password has to be at least 8 characters long</Form.Text>
+          </Grid>
+          <br />
+          <br />
+
+          <Grid item xs={12} sm={4} md={5}>
+            <Form.Label>Repeat password: </Form.Label>
+          </Grid>
+          <Grid item xs={12} sm={8} md={7}>
+            <Form.Control
+              size="sm"
+              type="password"
+              placeholder="Password"
+              minLength={8}
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+              isInvalid={validated && password !== confirmPassword}
+              required
+            />
+          </Grid>
+          <br />
+          <br />
+
+          <Grid item xs={12} sm={4} md={5}>
+            <Form.Label>Phone number: </Form.Label>
+          </Grid>
+          <Grid container item xs={12} sm={8} md={7}>
+            <Grid item xs={5} sm={4}>
+              <InputGroup size="sm">
+                <InputGroup.Prepend>
+                  <InputGroup.Text>+</InputGroup.Text>
+                </InputGroup.Prepend>
+                <Form.Control
+                  size="sm"
+                  type="text"
+                  pattern="[0-9]*"
+                  placeholder="47"
+                  onChange={(e) => setCountryCode("+" + e.target.value)}
+                  required
+                />
+              </InputGroup>
+            </Grid>
+
+            <Grid item xs={7} sm={8}>
+              <Form.Control
+                size="sm"
+                type="text"
+                pattern="[0-9]*"
+                minLength={8}
+                maxLength={17}
+                placeholder="99999999"
+                onChange={(e) => setPhoneNumber(e.target.value)}
+                required
+              />
+            </Grid>
+            <br />
+            <br />
+          </Grid>
+        </Grid>
+
+        <InternalButton
+          buttonStyle="btn-primary"
+          buttonSize="btn-large"
+          extraCss="apply-here"
+          type="submit"
+        >
+          Registrer
+        </InternalButton>
+      </Form>
+    </div>
   );
 };
