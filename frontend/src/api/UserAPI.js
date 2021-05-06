7447b1b7105a4032a2af60e6785593e24c233c39
skcheng <skcheng@stud.ntnu.no>
2021-05-06 18:47:16 +0200

    API to update user

--- frontend/src/api/UserAPI.js
+++ frontend/src/api/UserAPI.js
@@ -11,6 +11,23 @@ class UserAPI {
     const response = await client.get("user/self/");
     return response.data;
   }
+
+  /**
+   * Edits the currenly logged in user.
+   *
+   * @param user-the user object with may edited fields
+   * @returns the edited user
+   */
+
+  async editUser(user) {
+    const userResponse = await client.put("user/self/", {
+      email: user.email,
+      first_name: user.first_name,
+      last_name: user.last_name,
+      phone_number: user.phone_number,
+    });
+    return userResponse.data;
+  }
 }
 
 export default new UserAPI();
