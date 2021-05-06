2af9beec50406ee462456aa81960260f0ddbc0ed
skcheng <skcheng@stud.ntnu.no>
2021-05-06 15:04:20 +0200

    Made error message component

---
+++ frontend/src/components/error/ErrorHandler.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import { Alert } from "react-bootstrap";
+
+const warningStyle = {
+    fontSize: "12px",
+    padding: "5px 5px",
+    width: "inherit",
+  margin: "10px auto",
+  textAlign: "center",
+  };
+  
+
+/**
+ * A state for making simple error handling components. Supports
+ * simple error messages.
+ *
+ * @returns a newly created {@link ErrorState}
+ */
+export const useErrorState = () => {
+  const [error, setError] = useState(undefined);
+
+  const makeErrorComponent = () => {
+    if (!error) {
+      return null;
+    }
+
+    return (
+      <>
+        <Alert variant="warning" style={warningStyle}>{error}</Alert>
+      </>
+    );
+  };
+
+  return { error: makeErrorComponent(), setError };
+};
