1a00126c5ff3b39d5d889f3d24f262d08d1c25f5
skcheng <skcheng@stud.ntnu.no>
2021-05-09 11:26:08 +0200

    Centered text over form

--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -3,13 +3,14 @@ import { useAuthenticationPage } from "../core/auth";
 import { RegisterForm } from "../components/forms/RegisterForm";
 import { TextSection } from "../components/TextSection";
 import "./../App.css";
+import "./Register.css";
 
 export const Register = () => {
   useAuthenticationPage();
 
   return (
     <>
-      <div className="page">
+      <div className="register-page-container">
         <TextSection
           title="Register"
           text="Register an account to be able to sign up for trainings."
