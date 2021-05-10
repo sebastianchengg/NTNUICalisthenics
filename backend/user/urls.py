a16a5d7009cd15d0d90fe1eb28bdea7cf6d094ad
skcheng <skcheng@stud.ntnu.no>
2021-05-10 22:46:27 +0200

    Added url to reset password

--- backend/user/urls.py
+++ backend/user/urls.py
@@ -1,4 +1,5 @@
-from django.urls import path
+from django.urls import path, include
+from django.contrib.auth import views as auth_views
 from rest_framework_simplejwt import views as jwt_views
 from .views import (
     SelfAPIView,
@@ -12,4 +13,7 @@ urlpatterns = [
     path("register/", RegisterAPIView.as_view(), name="user-register"),
     path("self/", SelfAPIView.as_view(), name="user-self"),
     path("change/password/", ChangePasswordView.as_view(), name="change-password"),
+    path('password_reset/',
+         include('django_rest_passwordreset.urls', namespace='password_reset')),
+
 ]
