dcd8aeb0b53bafcfde455db94b8bdc9408291ba9
skcheng <skcheng@stud.ntnu.no>
2021-05-10 23:29:45 +0200

    Added css to improve looks

--- frontend/src/pages/ResetPasswordConfirm.js
+++ frontend/src/pages/ResetPasswordConfirm.js
@@ -1,13 +1,19 @@
 import React from "react";
 import { useParams } from "react-router";
 import { ResetPasswordConfirmForm } from "../components/forms/ResetPasswordConfirmForm";
+import { TextSection } from "../components/TextSection";
+import "./ResetPasswordConfirm.css";
 
 export const ResetPasswordConfirm = () => {
   const { token } = useParams();
 
   return (
     <>
-      <ResetPasswordConfirmForm token={token} />
+      <div className="reset-password-confirm-container">
+        <TextSection title="Reset password" text="Set your new password."/>
+        
+        <ResetPasswordConfirmForm token={token} />
+      </div>
     </>
   );
 };
