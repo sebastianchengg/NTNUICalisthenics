577e45d5545c1a02a0c0abedb458dcbdb516bf4c
Sebastian Cheng <sebast1908@gmail.com>
2021-08-02 12:51:36 +0200

    Created endpoint for updating trainings

--- backend/training/urls.py
+++ backend/training/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import (
     TrainingCreateAPIView,
+    TrainingUpdateAPIView,
+    training_detail,
     UserRegisterTrainingCreateAPIView,
     TrainingrelationUpdateAPIView,
     trainingrelation_delete,
@@ -12,11 +14,14 @@ urlpatterns = [
     path("create-training/", TrainingCreateAPIView.as_view(), name="create-training"),
     path("create-register/", UserRegisterTrainingCreateAPIView.as_view(),
          name="create-register"),
+    path("<str:pk>/", training_detail, name="training-detail"),
+    path("update/<str:training_id>/",
+         TrainingUpdateAPIView.as_view(), name="update-training"),
     path("update-relation/<str:user_id>-<str:training_id>/",
          TrainingrelationUpdateAPIView.as_view(), name="update-relation"),
     path("relation/delete/<str:user_id>-<str:training_id>/",
          trainingrelation_delete, name="trainingrelation-delete"),
-    path("<str:pk>/", training_register_detail, name="training-register"),
+    path("registered/<str:pk>/", training_register_detail, name="training-register"),
     path("list/registerable/", RegisterableTrainingView.as_view(),
          name="training-registerable"),
 ]
