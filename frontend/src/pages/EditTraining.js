a84785d4aea4ca88a5b06b1ee7795f652826e8a7
Sebastian Cheng <sebast1908@gmail.com>
2021-08-03 13:21:16 +0200

    Centered the form

--- frontend/src/pages/EditTraining.js
+++ frontend/src/pages/EditTraining.js
@@ -1,14 +1,19 @@
 import React from "react";
 import UpdateTrainingForm from "../components/forms/training/EditTrainingForm";
 import { useParams } from "react-router";
+import Grid from "@material-ui/core/Grid";
+import { useStaffAccessPage } from "../core/auth";
 
 export const EditTraining = () => {
+  useStaffAccessPage();
   const { id } = useParams();
 
   return (
     <>
       <div className="page">
-        <UpdateTrainingForm id={id}/>
+        <Grid container spacing={0} justifyContent="center">
+          <UpdateTrainingForm id={id} />
+        </Grid>
       </div>
     </>
   );
