eb9e73fb9772bd1d4350a45c08ba24dd9266f189
Sebastian Cheng <sebast1908@gmail.com>
2021-12-18 18:57:01 +0100

    Remove unused code

--- frontend/src/components/BackgroundPicture.js
+++ frontend/src/components/BackgroundPicture.js
@@ -3,7 +3,6 @@ import "./BackgroundPicture.css";
 
 //Makes the smooth animation of the title
 export const SmoothAnimation = () => {
-  document.body.classList.add("js-loading");
 
   function show() {
     document.body.classList.remove("js-loading");
