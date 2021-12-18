3f4ee1e62a95e3f4c3e1c56cc7212c2bd5585eaf
Sebastian Cheng <sebast1908@gmail.com>
2021-12-18 11:53:50 +0100

    Added boardmembers

--- frontend/src/pages/Board.js
+++ frontend/src/pages/Board.js
@@ -1,40 +1,53 @@
 import React from "react";
 import { HalfPicture } from "../components/BackgroundPicture";
 import { Boardcomponent } from "../components/BoardComponent";
-import { TextSection } from "../components/TextSection";
-import Grid from "@material-ui/core/Grid";
+import "./Board.css";
 import "../App.css";
 
 export const Board = () => {
   return (
     <>
-      <HalfPicture url="url-2" titleText="Board" />
-      <div className="page">
-        <TextSection
-          title="The board"
-          text="These are the dedicated people that run the group"
-        />
-        <Boardcomponent
-          url="image-leader"
-          direction={"left"}
-          name={"Sebastian Cheng"}
-          boardTitle={"Leader"}
-          about={"Main resposibility"}
-          favoriteExercise={"Front lever"}
-        />
+      <HalfPicture url="url-2" titleText="The Board" />
+      <div className="page board">
         <Boardcomponent
           url="image-leader"
           direction={"right"}
           name={"Sebastian Cheng"}
           boardTitle={"Leader"}
+          about={
+            "As leader I have the main responsibility of the group and coordinate the work of the board. I also represent the group externally."
+          }
+          favoriteExercise={"Front lever"}
+        />
+        <Boardcomponent
+          url="image-deputy"
+          direction={"left"}
+          name={"Anita Hundseid"}
+          boardTitle={"Deputy"}
           about={"Main resposibility"}
           favoriteExercise={"Front lever"}
         />
         <Boardcomponent
-          url="image-leader"
+          url="image-treasurer"
+          direction={"right"}
+          name={"Daniel Fremming"}
+          boardTitle={"Treasurer"}
+          about={"Main resposibility"}
+          favoriteExercise={"Front lever"}
+        />
+        <Boardcomponent
+          url="image-social-responsible-1"
           direction={"left"}
-          name={"Sebastian Cheng"}
-          boardTitle={"Leader"}
+          name={"Sven Thorkildsen"}
+          boardTitle={"Social responsible"}
+          about={"Main resposibility"}
+          favoriteExercise={"Front lever"}
+        />
+        <Boardcomponent
+          url="image-social-responsible-2"
+          direction={"right"}
+          name={"Louise Brose"}
+          boardTitle={"Social responsible"}
           about={"Main resposibility"}
           favoriteExercise={"Front lever"}
         />
